4ec7d740816d11502b9740b148cfe1e285a2998f
wu:wa~ <455300672@qq.com>
2022-09-13 00:03:39 +0800

    data construction done

--- Planner-release-ubuntu18/atri/atri.cpp
+++ Planner-release-ubuntu18/atri/atri.cpp
@@ -4,9 +4,13 @@
 using namespace _home;
 using namespace std;
 
+ostream &operator<<(ostream &os, shared_ptr<Object> obj);
+
 //////////////////////////////////////////////////////////////////////////
 ATRI::ATRI() : Plug("ATRI")
 {
+    robot = make_shared<Robot>();
+    objects.push_back(robot);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -16,7 +20,7 @@ void ATRI::Plan()
     PraseEnv(GetEnvDes());
 }
 
-void ATRI::PraseEnvSentence(const string &str)
+bool ATRI::PraseEnvSentence(const string &str)
 {
     int pos = 1;
     vector<string> words;
@@ -28,22 +32,91 @@ void ATRI::PraseEnvSentence(const string &str)
             pos = i + 1;
         }
     }
+    if (words.size() != 2 && words.size() != 3)
+        return false;
+
     if (words[0] == "hold")
     {
-        robot.hold = stoi(words[1]);
+        robot->hold = stoi(words[1]);
     }
     else if (words[0] == "plate")
     {
-        robot.plate = stoi(words[1]);
+        robot->plate = stoi(words[1]);
     }
-    else if (words[0] == "at")
+    else
     {
+        int ind = stoi(words[1]);
+        int last_size = objects.size();
+        while (ind >= last_size)
+        {
+            objects.push_back(make_shared<Object>(last_size++));
         }
+        if (words[0] == "opened")
+        {
+            auto p = dynamic_pointer_cast<Container>(objects[ind]);
+            if (p == nullptr)
+                return false;
+            p->isOpen = true;
+        }
+        else if (words[0] == "closed")
+        {
+            auto p = dynamic_pointer_cast<Container>(objects[ind]);
+            if (p == nullptr)
+                return false;
+            p->isOpen = false;
+        }
+        if (words[0] == "at")
+        {
+            objects[ind]->location = stoi(words[2]);
+        }
+        else if (words[0] == "sort")
+        {
+            objects[ind]->sort = words[2];
+        }
+        else if (words[0] == "size")
+        {
+            if (words[2] == "big")
+            {
+                auto p = make_shared<BigObject>(objects[ind]);
+                bigObjects.push_back(p);
+                objects[ind] = p;
+            }
+            else if (words[2] == "small")
+            {
+                auto p = make_shared<SmallObject>(objects[ind]);
+                smallObjects.push_back(p);
+                objects[ind] = p;
+            }
+        }
+        else if (words[0] == "color")
+        {
+            auto p = dynamic_pointer_cast<SmallObject>(objects[ind]);
+            if (p == nullptr)
+                return false;
+            p->color = words[2];
+        }
+        else if (words[0] == "inside")
+        {
+            auto p = dynamic_pointer_cast<SmallObject>(objects[ind]);
+            if (p == nullptr)
+                return false;
+            p->inside = stoi(words[2]);
+        }
+        else if (words[0] == "type")
+        {
+            if (words[2] == "container")
+            {
+                auto p = make_shared<Container>(objects[ind]);
+                containers.push_back(p);
+                objects[ind] = p;
+            }
+        }
+    }
+    return true;
 }
 
-void ATRI::PraseEnv(const string &env)
+bool ATRI::PraseEnv(const string &env)
 {
-    vector<string> div;
     regex reg("\\(.*?\\)");
     cmatch m;
     auto pos = env.data() + 1;
@@ -52,11 +125,58 @@ void ATRI::PraseEnv(const string &env)
     {
         string str = m.str();
         cout << str << endl;
-        PraseEnvSentence(str);
+        if (PraseEnvSentence(str) == false)
+            return false;
     }
+    for (int i = 0; i < smallObjects.size(); i++)
+    {
+        if (smallObjects[i]->inside != UNKNOWN)
+        {
+            dynamic_pointer_cast<Container>(objects[smallObjects[i]->inside])->smallObjectsInside.push_back(smallObjects[i]);
+        }
+        if (smallObjects[i]->location != UNKNOWN)
+        {
+            for (auto big : bigObjects)
+            {
+                if (big->location == smallObjects[i]->location)
+                {
+                    big->smallObjectsOn.push_back(smallObjects[i]);
+                    break;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < objects.size(); i++)
+    {
+        cout << objects[i] << endl;
+    }
+    return true;
 }
 
 void ATRI::Fini()
 {
     cout << "#(ATRI): Fini" << endl;
+}
+ostream &operator<<(ostream &os, shared_ptr<Object> obj)
+{
+    if (dynamic_pointer_cast<SmallObject>(obj) != nullptr)
+    {
+        os << "SmallObject " << dynamic_pointer_cast<SmallObject>(obj)->ToString();
+    }
+    else if (dynamic_pointer_cast<Robot>(obj) != nullptr)
+    {
+        os << "Robot " << dynamic_pointer_cast<Robot>(obj)->ToString();
+    }
+    else if (dynamic_pointer_cast<BigObject>(obj) != nullptr)
+    {
+        if (dynamic_pointer_cast<Container>(obj) != nullptr)
+        {
+            os << "Container " << dynamic_pointer_cast<Container>(obj)->ToString();
+        }
+        else
+        {
+            os << "BigObject  " << dynamic_pointer_cast<BigObject>(obj)->ToString();
+        }
+    }
+    return os;
 }
--- Planner-release-ubuntu18/atri/atri.hpp
+++ Planner-release-ubuntu18/atri/atri.hpp
@@ -5,9 +5,7 @@
  * Affiliation: Multi-Agent Systems Lab.
  *              University of Science and Technology of China
  */
-
-#ifndef __home_atri_HPP__
-#define __home_atri_HPP__
+#pragma once
 
 #include "cserver/plug.hpp"
 #include "string"
@@ -16,10 +14,86 @@ using namespace std;
 #define UNKNOWN -1
 namespace _home
 {
-    class Object;
-    class SmallObject;
-    class BigObject;
-    class Container;
+    class Object
+    {
+    public:
+        int location;
+        int id;
+        string sort = "";
+        Object(int id, const string &sort = "", int location = UNKNOWN) : sort(sort), location(location), id(id) {}
+        Object();
+        virtual string ToString()
+        {
+            return "id:" + to_string(id) + "    at:" + to_string(location) + "     sort:" + sort + "\n";
+        }
+        ~Object() {}
+    };
+
+    class Robot : public Object
+    {
+    public:
+        int hold;
+        int plate;
+
+        Robot(int id, int location = UNKNOWN, int hold = UNKNOWN, int plate = UNKNOWN) : Object(id, "robot", location), hold(hold), plate(plate) {}
+        Robot() : Robot(0) {}
+        virtual string ToString() override
+        {
+            return Object::ToString() + "hold:" + to_string(hold) + "     plate:" + to_string(plate) + "\n";
+        }
+        ~Robot() {}
+    };
+    class SmallObject : public Object
+    {
+    public:
+        string color = "";
+        int inside = UNKNOWN;
+        SmallObject(int id, int location = UNKNOWN, const string &sort = "", const string &color = "") : Object(location, sort, id), color(color) {}
+        SmallObject(shared_ptr<Object> obj) : Object(*obj) {}
+        virtual string ToString() override
+        {
+            return Object::ToString() + "color:" + color + "    inside:" + to_string(inside) + "\n";
+        }
+        ~SmallObject() {}
+    };
+
+    class BigObject : public Object
+    {
+    public:
+        vector<shared_ptr<SmallObject>> smallObjectsOn;
+        BigObject(int id, int location = UNKNOWN, string sort = "") : Object(location, sort, id) {}
+        BigObject(shared_ptr<Object> obj) : Object(*obj) {}
+        virtual string ToString() override
+        {
+            string out = Object::ToString() + "Small Objects On:\n";
+            for (int i = 0; i < smallObjectsOn.size(); i++)
+            {
+                out += to_string(i) + " " + smallObjectsOn[i]->ToString();
+            }
+            return out;
+        }
+        ~BigObject() {}
+    };
+
+    class Container : public BigObject
+    {
+    public:
+        vector<shared_ptr<SmallObject>> smallObjectsInside;
+        bool isOpen;
+        Container(int id, int location = UNKNOWN, bool isOpen = true, string sort = "") : BigObject(id, location, sort), isOpen(isOpen) {}
+        Container(shared_ptr<Object> obj) : BigObject(obj) {}
+        Container(shared_ptr<BigObject> obj) : BigObject(*obj) {}
+        virtual string ToString() override
+        {
+            string out = BigObject::ToString() + "Small Objects Inside:\n";
+            for (int i = 0; i < smallObjectsInside.size(); i++)
+            {
+                out += to_string(i) + " " + smallObjectsInside[i]->ToString();
+            }
+            return out;
+        }
+        ~Container() {}
+    };
 
     class ATRI : public Plug
     {
@@ -28,63 +102,16 @@ namespace _home
 
     protected:
         void Plan();
-        unordered_map<int, shared_ptr<SmallObject>> smallObjects;
-        unordered_map<int, shared_ptr<Container>> containers;
-        unordered_map<int, shared_ptr<BigObject>> bigObjects;
-        Robot robot;
-        void PraseEnv(const string &env);
-        void PraseEnvSentence(const string &str);
+        vector<shared_ptr<Object>> objects;
+        vector<shared_ptr<SmallObject>> smallObjects;
+        vector<shared_ptr<Container>> containers;
+        vector<shared_ptr<BigObject>> bigObjects;
+        shared_ptr<Robot> robot;
+        bool PraseEnv(const string &env);
+        bool PraseEnvSentence(const string &str);
         void Fini();
     }; // Plug
 
-    class Object
-    {
-    public:
-        int location;
-        int id;
-        Object(int id, int location = UNKNOWN) : location(location), id(id) {}
-        Object(int id) : id(id) {}
-        ~Object() {}
-
-    private:
-        Object();
-    };
-
-    class Robot : Object
-    {
-    public:
-        int hold;
-        int plate;
-
-        Robot(int id, int location = UNKNOWN, int hold = UNKNOWN, int plate = UNKNOWN) : Object(location, id), hold(hold), plate(plate) {}
-        Robot() : Robot(0) {}
-        ~Robot() {}
-    };
-    class SmallObject : Object
-    {
-    public:
-        string sort;
-        string color;
-        SmallObject(int id, int location = UNKNOWN, string sort = "") : Object(location, id), sort(sort) {}
-    };
-
-    class BigObject : Object
-    {
-    public:
-        string sort;
-        vector<shared_ptr<SmallObject>> smallObjectsOn;
-        BigObject(int id, int location = UNKNOWN, string sort = "") : Object(location, id), sort(sort) {}
-    };
-
-    class Container : Object
-    {
-    public:
-        string sort;
-        vector<shared_ptr<SmallObject>> smallObjectsInside;
-        bool isOpen;
-        Container(int id, int location = UNKNOWN, bool isOpen = true, string sort = "") : Object(location, id), sort(sort), isOpen(isOpen) {}
-    };
 } //_home
 
-#endif //__home_atri_HPP__
 // end of file
